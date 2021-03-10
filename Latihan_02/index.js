426320ac24772549df052f7f92a45cc9b9f02ca4
JoshuaSandag <s21810416@student.unklab.ac.id>
2021-03-10 20:55:32 +0700

    first commit

---
+++ Latihan_02/index.js
@@ -0,0 +1,52 @@
+//Nama : Sandag, Joshua
+
+//Nomor 1
+// mandi = () =>{
+//     console.log("mandi");
+// }
+// sarapan = (callback) =>{
+//     setTimeout(() => {
+//         console.log("sarapan");
+//         callback();
+//     },3000);
+//     }
+// berangkatSekolah = () =>{
+//     console.log("berangkat sekolah");
+// }
+// mandi();
+// sarapan(berangkatSekolah);
+
+
+//Nomor 2
+// helloWorld =()=> {
+//     return helloWorld = new Promise((resolve) => {
+//             resolve("Hello World");
+//     })
+// };
+// let messsages = async () => {
+//     try{
+//         const msg = await helloWorld();
+//         setTimeout(() => {
+//             console.log(msg);
+//         },2000);
+//     }
+//     catch(error)
+//     {
+//         console.log(error);
+//     }
+//     };
+// messsages()
+
+
+//Nomor 3
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (user) {
+//     console.log(user);
+//   });
+
+// ambilDataUser = () => {
+// }
+// ambilDataUser()
