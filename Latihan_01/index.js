88ef9601a06793977e4fec698728f41aff9c143e
JoshuaSandag <s21810416@student.unklab.ac.id>
2021-03-10 20:33:54 +0700

    first commit
    membuat index.js dan index.html

---
+++ Latihan_01/index.js
@@ -0,0 +1,77 @@
+// Nama : Sandag, Joshua
+// 1. Array Exercises
+
+/*
+let people = ["Greg", "Mary", "Devon", "James"];
+
+for (let i = 0; i < people.length; i++ ){
+    console.log(people[i]);
+}; Nomor 1
+
+people.forEach (function(item){
+    console.log(item);
+}); Nomor 2
+
+people.shift("Greg");
+console.log(people); Nomor 3
+
+people.pop("James");
+console.log(people); Nomor 4
+
+people.unshift("Matt");
+console.log(people); Nomor 5
+
+people.push("Joshua");
+console.log(people); Nomor 6
+
+for(let i=0; i<people.length; i++){
+    console.log(people[i]);
+    if(people[i]==="Mary"){
+        break;
+    }
+} Nomor 7
+
+let copyPeople = people.slice(2);
+console.log(copyPeople);  Nomor 8
+
+let newArray = ["Greg", "Mary","Devon", "James"];
+newArray.splice(2,1,"Elizabeth","Artie");
+console.log(newArray); Nomor 9
+
+let withbob = people.concat("bob");
+console.log(withbob); Nomor 10
+*/
+
+//2. Object Exercises
+/*
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+};
+
+programming.languages.push("Go");
+console.log(programming); Nomor 1
+
+programming['difficulty'] = 7;
+console.log(programming); Nomor 2 
+
+delete programming.jokes;
+console.log(programming); Nomor 3
+
+programming.isFun = true;
+console.log(programming); Nomor 4
+    
+let newProgramming = programming.languages.map(function(item, index)
+    {
+        return index + "-" + item;
+    });
+    console.log(newProgramming); Nomor 5
+*/
+
+
+
+
+
